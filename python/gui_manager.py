e5c26b40f38b0b2e4fc2d37391646f0888e3e829
RoyKitagawa <v.roy.kitagawa@gmail.com>
2023-03-30 02:24:51 +0900

    create until the basic UI placements

--- python/gui_manager.py
+++ python/gui_manager.py
@@ -1,4 +1,7 @@
+import tkinter
 from tkinter import ttk
+from tkinter import colorchooser
+from tkhtmlview import HTMLLabel
 
 # 資料
 # https://www.python-beginners.com/entry/20210607/1623057987
@@ -19,12 +22,200 @@ from tkinter import ttk
 # 5. サンプル表示
 #     最新の設定をベースに現在日時の時計を実際に表示する
 
+class ColorSelectButton(tkinter.Button):
+    def __init__(self, master):
+        super().__init__(
+            master=master,
+            text="",
+            width=10,
+            relief=tkinter.SOLID,
+            bd=1,
+            command=self.onclick_color_button,
+        )
 
-def create_date_time_ui(root):
-    
-    item_list = ["a", "b", "c"]
+    def onclick_color_button(self):
+        self.config(
+            # カラピッカー呼出し後、選択された色の第二要素（色コード）を反映させる
+            bg=colorchooser.askcolor()[1]
+        )
+
+def create_datetime_sample(root):
+    frame = ttk.LabelFrame(root, text="サンプル表示")
+
+    content_frame = ttk.Label(frame,background='#778899')
+    content_frame.pack(
+        fill=tkinter.BOTH,
+        expand=True,
+        padx=10,
+        pady=(10, 10),
+        anchor=tkinter.CENTER,
+    )
+
+    # 日時の表示（サンプルのためリアルタイムでなくてOK）
+    datetime_label = ttk.Label(content_frame, background='#778899', text="1月23日（金）\n12:34:56.012")
+    datetime_label.pack(
+        anchor=tkinter.CENTER,
+        expand=True,
+    )
+
+    frame.pack(
+        anchor=tkinter.N,
+        fill=tkinter.BOTH,
+        padx=10,
+        pady=(10, 10),
+        expand=True,
+    )
+
+def create_background_color(root):
+    frame = ttk.Frame(root)
+
+    # Label作成
+    title_frame = ttk.Frame(frame, width=110, height=20,)
+    ttk.Label(title_frame, text="背景色",).pack(side=tkinter.RIGHT,)
+    title_frame.propagate(False)
+    title_frame.pack(
+        side=tkinter.LEFT,
+        padx=(0, 10),
+    )
+
+    # カラピッカー用ボタン
+    color_button = ColorSelectButton(frame)
+    color_button.pack(
+        side=tkinter.LEFT,
+    )
+
+    frame.pack(
+        anchor=tkinter.N,
+        fill=tkinter.X,
+        padx=10,
+        pady=(10, 0),
+    )
+
+
+def create_font_color(root):
+    frame = ttk.Frame(root)
+
+    # Label作成
+    title_frame = ttk.Frame(frame, width=110, height=20,)
+    ttk.Label(title_frame, text="文字色",).pack(side=tkinter.RIGHT,)
+    title_frame.propagate(False)
+    title_frame.pack(
+        side=tkinter.LEFT,
+        padx=(0, 10),
+    )
+
+    # カラピッカー用ボタン
+    color_button = ColorSelectButton(frame)
+    # color_button = tkinter.Button(
+    #     frame,
+    #     text="色",
+    #     command=onclick_color_button,
+    #     height=1,
+    #     width=10,
+    #     bg="red",)
+    color_button.pack(
+        side=tkinter.LEFT,
+    )
+    # # テキスト入力エリア
+    # entry = tkinter.Entry(frame,)
+    # entry.pack(
+    #     side=tkinter.LEFT,
+    # )
+
+    frame.pack(
+        anchor=tkinter.N,
+        fill=tkinter.X,
+        padx=10,
+        pady=(10, 0),
+    )
+
+def onclick_color_button():
+    return
+
+def create_font_size(root):
+    frame = ttk.Frame(root)
+
+    # Label作成
+    title_frame = ttk.Frame(frame, width=110, height=20,)
+    ttk.Label(title_frame, text="フォントサイズ",).pack(side=tkinter.RIGHT,)
+    title_frame.propagate(False)
+    title_frame.pack(
+        side=tkinter.LEFT,
+        padx=(0, 10),
+    )
+
+    # テキスト入力エリア
+    entry = tkinter.Entry(frame,)
+    entry.pack(
+        side=tkinter.LEFT,
+    )
+
+    frame.pack(
+        anchor=tkinter.N,
+        fill=tkinter.X,
+        padx=10,
+        pady=(10, 0),
+    )
+
+
+def create_font_ui(root):
+    frame = ttk.Frame(root)
+
+    # Label作成
+    title_frame = ttk.Frame(frame, width=110, height=20,)
+    ttk.Label(title_frame, text="フォント",).pack(side=tkinter.RIGHT,)
+    title_frame.propagate(False)
+    title_frame.pack(
+        side=tkinter.LEFT,
+        padx=(0, 10),
+    )
+
+    # Combobox自体を作成
+    item_list = tkinter.font.families()
     combobox = ttk.Combobox(
-        master = root,
+        master = frame,
         values = item_list,
     )
-    return
+    combobox.pack(        
+        side=tkinter.LEFT,
+        fill=tkinter.X,
+        expand=True,
+        )
+    
+    frame.pack(
+        anchor=tkinter.N,
+        fill=tkinter.X,
+        padx=10,
+        pady=(10, 0),
+    )
+
+def create_date_time_ui(root):
+    frame = ttk.Frame(root)
+
+    # Label作成
+    title_frame = ttk.Frame(frame, width=110, height=20)
+    ttk.Label(title_frame, text="日時表記フォーマット",).pack(side=tkinter.RIGHT,)
+    title_frame.propagate(False)
+    title_frame.pack(
+        side=tkinter.LEFT,
+        padx=(0, 10),
+    )
+
+    # Combobox自体を作成
+    item_list = ["a", "b", "c"]
+    combobox = ttk.Combobox(
+        master = frame,
+        values = item_list,
+    )
+    combobox.pack(        
+        side=tkinter.LEFT,
+        fill=tkinter.X,
+        expand=True,
+        )
+    
+    frame.pack(
+        anchor=tkinter.N,
+        fill=tkinter.X,
+        padx=10,
+        pady=(10, 0),
+    )
--- python/settings.py
+++ python/settings.py
@@ -1,4 +1,5 @@
 import tkinter
+from tkinter import ttk
 import gui_manager as gui
 
 # Tkクラス生成
@@ -9,7 +10,13 @@ root.geometry('600x400')
 root.title('サンプル画面')
 
 gui.create_date_time_ui(root)
+gui.create_font_ui(root)
+gui.create_font_size(root)
+gui.create_font_color(root)
+gui.create_background_color(root)
+gui.create_datetime_sample(root)
 
+frame = ttk.Frame(root)
 
 # 画面をそのまま表示
 root.mainloop()
